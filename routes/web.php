f7389f7cfb22a50c23fae278b637d168270e6784
odhi99 <odhii9671@gmail.com>
2024-09-04 01:47:41 +0800

    fortify, login - logout

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
 });
 
-Route::get('/dashboard', function () {
-    return view('pages.dashboard', ['type_menu' => 'dashboard']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('dashboard', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('dashboard');
+
+    // Route::resource('user', UserController::class);
 });
